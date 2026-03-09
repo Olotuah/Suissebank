4a994ece94e782e22ad002209032c2c10d8b38ee
Gbenga Olotuah <74839657+Olotuah@users.noreply.github.com>
2026-03-09 07:46:43 +0100

    Update statementRoutes.js

--- backend/routes/statementRoutes.js
+++ backend/routes/statementRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
 import authMiddleware from "../middleware/authMiddleware.js";
-import { generateStatementPdf } from "../controllers/statementController.js";
+import {
+  previewStatement,
+  generateStatementPdf,
+} from "../controllers/statementController.js";
 
 const router = express.Router();
 
+router.get("/preview", authMiddleware, previewStatement);
 router.get("/pdf", authMiddleware, generateStatementPdf);
 
 export default router;
